b11139bd965e54334e2480cea8f662c8b9421e3f
mohammadraziei <mohammadraziei1375@gmail.com>
2020-01-23 10:57:33 +0330

    commit some changes.

--- Matlab-HW01/Codes/Q2/main.m
+++ Matlab-HW01/Codes/Q2/main.m
@@ -18,7 +18,7 @@ ax.XTick = 0:.2:2;
 
 xlabel('Normalized Frequency (\times\pi rad/sample)')
 ylabel('Magnitude (dB)')
-% armax(data,
+
 
 %% B
 Num = 10000;
@@ -45,22 +45,54 @@ for i = 1:10
     AIC = [AIC, aic_raw];
 end
 figure;
+subplot(121);
 plot(Error)
 title("levinson")
-figure;
+subplot(122);
 plot(AIC)
 title("AIC")
 % [a,e] = levinson(r,n);
 
 %% D
-% sys = ar(data,5);
 [Pxx,w_D] = pyulear(Y,5);
 figure;hold on
 plot(w(1:1000)/pi,20*log10(abs(h(1:1000))))
 plot(w_D/pi,10*log10(Pxx))
 legend('True Power Spectral Density','pyulear PSD Estimate')
+xlabel('Normalized Frequency (\times\pi rad/sample)')
+ylabel('Magnitude (dB)')
 
 %% E
 
+% approach — Algorithm for computing AR model
+% 'fb' (default) | 'burg' | 'gl' | 'ls' | 'yw'
+% Algorithm for computing the AR model, specified as one of the following values:
+% 
+% 'burg': Burg's lattice-based method. Solves the lattice filter equations using the harmonic mean of forward and backward squared prediction errors.
+% 'fb': (Default) Forward-backward approach. Minimizes the sum of a least-squares criterion for a forward model, and the analogous criterion for a time-reversed model.
+% 'gl': Geometric lattice approach. Similar to Burg's method, but uses the geometric mean instead of the harmonic mean during minimization.
+% 'ls': Least-squares approach. Minimizes the standard sum of squared forward-prediction errors.
+% 'yw': Yule-Walker approach. Solves the Yule-Walker equations, formed from sample covariances.
 
-%% F
+ar_alg = ["burg","fb","gl","ls","yw"];
+ar_A = [];
+for i = 1:length(ar_alg)
+    sys = ar(data,5,ar_alg(i));
+    ar_A = [ar_A;sys.A];
+end
+%%
+figure;hold on    
+plot(w(1:1e3)/pi,20*log10(abs(h(1:1e3))),'r');
+legend_cell = cell(1,length(ar_alg)+2);
+legend_cell{1} = 'True Power Spectral Density';
+for i = 1:length(ar_alg)
+    [h_E,w_E] = freqz(1,ar_A(i,:),'whole',2001);
+    plot(w_E(1:1e3)/pi,20*log10(abs(h_E(1:1e3))));
+    legend_cell{i+1} = ['AR Power Estimate with "' char(ar_alg(i)) '"'];
+end
+plot(w_D/pi,10*log10(Pxx))
+legend_cell{i+2} = 'pyulear PSD Estimate';
+legend(legend_cell)
+xlabel('Normalized Frequency (\times\pi rad/sample)')
+ylabel('Magnitude (dB)')
+ylim([-50,100])
--- Matlab-HW01/Codes/Q4/main.m
+++ Matlab-HW01/Codes/Q4/main.m
@@ -1,5 +1,21 @@
 disp('Question 4 is running ...');
 %% global variables or settings
-
-
+addpath(fullfile('..','OSET'), fullfile('../OSET','Tools'), fullfile('../../../','DataSets'));%, fullfile('../OSET','Databases'))
+addpath(fullfile('../OSET/Tools/Mex Files','Filters'),fullfile('../OSET/Tools/Mex Files','MedianFilter3'),fullfile('../OSET/Tools/Mex Files','TrimmedFilter3'))
 %% A
+load('Signal1.mat');
+fs = Signal1.fs;
+ECG1 = Signal1.ECG - mean(Signal1.ECG);
+figure;
+subplot(121);plot(ECG1);
+subplot(122);pwelch(ECG1);
+% b = TrimmedFilter(ECG1,'median',(.2*fs));
+% b = TrimmedFilter(b,'median',(.4*fs));
+% b = LPFilter(b,10/fs);
+b = mean(ECG1);
+ECG2 = ECG1 - b;
+
+% Remove civil noise
+[num,den] = iirnotch(60/(fs/2),0.25/(fs/2));
+ECG3 = filter(num,den,ECG2);
+figure; pwelch(ECG3);
